4eb704a52ab610909be68e9908f87bd16ff7b750
petrovale <leha.isakov@yandex.ru>
2019-09-22 15:19:08 +0300

    adding spring-restdocs

--- src/main/java/guru/springboot/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/guru/springboot/msscbeerservice/web/model/BeerDto.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -28,6 +29,7 @@ public class BeerDto {
   private OffsetDateTime lastModifiedDate;
 
   @NotBlank
+  @Size(min = 3, max = 100)
   private String beerName;
 
   @NotNull
@@ -41,6 +43,7 @@ public class BeerDto {
   @NotNull
   private BigDecimal price;
 
+  @Positive
   private Integer quantityOnHand;
 
 }
