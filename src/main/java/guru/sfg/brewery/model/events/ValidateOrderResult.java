14df0605540d1cfc0fbc278e178f9fd556457cb3
petrovale <leha.isakov@yandex.ru>
2020-02-23 16:47:22 +0300

    add validation order listener

---
+++ src/main/java/guru/sfg/brewery/model/events/ValidateOrderResult.java
@@ -0,0 +1,16 @@
+package guru.sfg.brewery.model.events;
+
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ValidateOrderResult {
+  private UUID orderId;
+  private Boolean isValid;
+}
